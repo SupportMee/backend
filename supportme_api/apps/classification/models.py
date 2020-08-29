facd1c92e0e15712c957f7b841fd201dec94a8ed
dianmoli98 <dianmoli@espol.edu.ec>
2020-08-29 14:59:54 -0500

    modified  unique in name cities and categories

--- supportme_api/apps/classification/models.py
+++ supportme_api/apps/classification/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Category, City
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=100, unique=True)
+    
     def __str__(self):
         return self.name
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+
     def __str__(self):
         return self.name

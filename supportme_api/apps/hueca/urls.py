1675f813518ab5417977710fe3ab7e19b678e7a2
dianmoli98 <dianmoli@espol.edu.ec>
2020-08-30 03:01:33 -0500

    Add Get,Post and Delete in Image

--- supportme_api/apps/hueca/urls.py
+++ supportme_api/apps/hueca/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
 	path('huecas/', views.huecas, name="huecas-list"),
 	path('hueca/<int:pk>/', views.hueca, name="hueca-single"),
-	path('image/<int:hueca>/', views.image, name="image-hueca"),
+
+	path('images/<int:hueca>/', views.images, name="image-hueca-list"),
+	path('image/<int:pk>/', views.image, name="image-hueca"),
+	path('image/', views.post_image, name="image-post"),
 ]
--- supportme_api/apps/hueca/views.py
+++ supportme_api/apps/hueca/views.py
@@ -3,8 +3,43 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from rest_framework import status
+from rest_framework import generics
+
 from .models import Hueca, Menu, Image
 from .serializers import HuecaSerializer, MenuSerializer, ImageSerializer
+
+#IMAGES
+
+# response  a single Image
+@api_view(['GET','DELETE'])
+def image(request, pk):
+    data = generics.get_object_or_404(Image,id=pk)
+    #data = Image.objects.get(hueca=hueca)
+    if(request.method=='GET'):
+        serializer = ImageSerializer(data, many=False)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    else:
+        data.delete()
+        return Response('Item succsesfully delete!',status=status.HTTP_200_OK)  
+
+# response  of  image
+@api_view(['POST'])
+def post_image(request):
+        serializer = ImageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)  
+
+
+# response list of images
+@api_view(['GET'])
+def images(request,hueca):
+    data = Image.objects.filter(hueca=hueca).all()
+    serializer = ImageSerializer(data, many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
 
 
 # response list of huecas
@@ -23,9 +58,5 @@ def hueca(request, pk):
     return Response(serializer.data)
 
 
-# response  a single Image
-@api_view(['GET'])
-def image(request, hueca):
-    data = Image.objects.get(hueca=hueca)
-    serializer = ImageSerializer(data, many=False)
-    return Response(serializer.data)
+
+
